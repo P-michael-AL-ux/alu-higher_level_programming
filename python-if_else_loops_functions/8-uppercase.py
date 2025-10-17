7a7549987213af1a6d0697ead6bd027ccf3e85a0
Panom M <p.makuei@alustudent.com>
2025-10-17 08:26:23 -0700

    Python loops one of the best

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/pyhton3
+def uppercase(str):
+    result = ""
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+           i = chr(ord(i) - 32)
+        print("{}".format(i), end = "")
+    print("")
