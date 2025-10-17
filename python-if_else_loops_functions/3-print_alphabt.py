a9bf2db177548068f955c21961c8dcaba1955573
Panom M <p.makuei@alustudent.com>
2025-10-17 10:05:09 -0700

    Update on file 3-print_alpa

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if chr(i) != 'q' and chr(i) != 'e':
-        print("{}".format(chr(i)), end = '')
+    char = chr(i)
+    if char != 'q' and char != 'e':
+        print(char, end='')
+print()
 
